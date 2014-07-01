5dd422f9cf9340e6997f4efcba54924925aa38c8
Christian Rackerseder <git@echooff.de>
2014-07-01 18:35:21 +0200

    Removed Python 3.2 from build

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
         'Topic :: Software Development :: Libraries :: Python Modules'
